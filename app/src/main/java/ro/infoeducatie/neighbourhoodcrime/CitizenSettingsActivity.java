22f80e5efed67c9dbe25e9e36dfb7ff6b9ee346f
RaresScoica <40122951+RaresScoica@users.noreply.github.com>
2019-06-03 19:47:04 +0300

    Design

--- app/src/main/java/ro/infoeducatie/neighbourhoodcrime/CitizenSettingsActivity.java
+++ app/src/main/java/ro/infoeducatie/neighbourhoodcrime/CitizenSettingsActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
